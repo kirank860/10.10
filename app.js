19a84bed47b9dc2f328b26557964dbea52d80543
kirank860 <kkiru889@gmail.com>
2024-04-28 16:21:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var aboutRouter =require('./routes/about')
+var serviceRouter =require('./routes/service')
+var contactRouter = require('./routes/contact')
+var interiorRouter =require('./routes/interior')
+var exteriorRouter =require('./routes/exterior')
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/service', serviceRouter);
+app.use('/contact', contactRouter);
+app.use('/interior', interiorRouter);
+app.use('/exterior',exteriorRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
